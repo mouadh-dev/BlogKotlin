cc790c4a4d7f49b4e53f4ff87485272ced8ea6bb
mouadh-dev <mouadh.laa@gmail.com>
2022-10-31 16:27:23 +0100

    design home

---
+++ app/src/main/java/com/example/Blog/Entity/UserItemEntity.kt
@@ -0,0 +1,15 @@
+package com.example.Blog.Entity
+
+import android.os.Parcel
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+//@Entity(tableName = "user_table")
+//data class UserItemEntity {
+//    @PrimaryKey(autoGenerate = true)
+//    val id: Int,
+//    val fullName: String,
+//
+//}
